53838a2c619c6d037526cb8885c66fdb04559ef4
zcl <28928400@qq.com>
2013-08-16 10:20:29 +0800

    test

--- Send.js
+++ Send.js
@@ -2,47 +2,56 @@ var http = require('http');
 var querystring = require('querystring');
 var cookie = require('./GetCookie');
 var db = require('./db');
+var async=require('async');
+var id, cookies, token;
 
-var id, cookies, token, arr, lengths;
-var i = 1;
-var foo;
-exports.fs = function (_id, callback) {
-    foo = callback;
+
+exports.fs = function (_id, callbacks) {
+
     cookie.cookie(function (_cookie, _token) {
         id = _id;
         cookies = _cookie;
         token = _token;
         db.find(function (result) {
-            arr = result.split(',');
-            lengths = arr.length;
+           var arr = result.split(',');
+          //lengths = arr.length;
             //interval(callback);
-            b();
+            async.forEachSeries(arr, function(item, callback) {
+                console.log('1.3 enter: ' + item);
+                setTimeout(function(){
+                    sendmsg(item)   ;
+                    callback(null, item);
+                }, 1000);
+            }, function(err) {
+                console.log('1.3 err: ' + err);
+                callbacks('err') ;
+            });
 
         })
 
     });
 }
 
-var t = -1;
-var b = function () {
-    if (t > -1) {
-        clearTimeout(t);
-    }
-    t = setTimeout(function () {
-        if (i == 6) {
-            clearTimeout(t);
-            foo("ok");
-            return;
-        } else {
-            sendmsg(arr[i]);
-            i++;
-            b();
-        }
+//var t = -1;
+//var b = function () {
+//    if (t > -1) {
+//        clearTimeout(t);
+//    }
+//    t = setTimeout(function () {
+//        if (i == 6) {
+//            clearTimeout(t);
+//            foo("ok");
+//            return;
+//        } else {
+//            sendmsg(arr[i]);
+//            i++;
+//            b();
+//        }
+//
+//    }, 1000);
+//}
 
-    }, 1000);
-}
-
-var sendmsg = function (userid,callback) {
+var sendmsg = function (userid) {
     var post = querystring.stringify({
         type: '10',
         fid: id,
