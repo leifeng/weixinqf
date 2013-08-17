78f121ce5a73ebc272ca223628b24c01c808b77a
zcl <28928400@qq.com>
2013-08-17 15:23:11 +0800

    优化

--- app.js
+++ app.js
@@ -1,38 +1,25 @@
 var http = require('http');
 var url = require("url");
 var querystring = require('querystring');
-var collect = require('./collect');
-var send = require('./Send');
-var test = require('./test');
-http.createServer(function (req, res) {
+var lib = require('./lib/req');
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+http.createServer(function (req, res) {
+    var id = null, type = null;
+    res.writeHead(200, {'Content-Type': 'text/html'});
     var query = url.parse(req.url).query;
-    var type = querystring.parse(query).type;
-    var id = querystring.parse(query).id;
-    if (type != '' && type != null) {
-        if (type == 'cj') {
-            collect.cj(function (result) {
-                res.end(result);
-            });
-        } else if (type == 'fs') {
-            if (id != '' && id != null) {
-                send.fs(id, function (result) {
-                    res.end(result);
-                });
-            } else {
-                res.end('fs bad');
-            }
-        } else if (type == 'test') {
-            test.fs(function (result) {
-                res.end(result);
-            });
-        }
-        else {
-            res.end('hello world!');
-        }
+    type = querystring.parse(query).type;
+    id = querystring.parse(query).id;
+    if (type == "cj") {
+        lib.collect(function () {
+            res.end("采集完成");
+        });
+    } else if (type == "fs" && id != null) {
+        lib.send(function () {
+            res.end("发送处理中..");
+        });
     } else {
         res.end('hello world!');
     }
+
 }).listen(3002);
 console.log('runing at 3002');
