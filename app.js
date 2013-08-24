cee6942bd19807e75484aa3ccaa642e27cb43d25
zcl <28928400@qq.com>
2013-08-24 10:52:42 +0800

    加入状态

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 var url = require("url");
 var querystring = require('querystring');
 var lib = require('./lib/req');
-
+var status = require('./lib/status');
 http.createServer(function (req, res) {
     var id = null, type = null;
     res.writeHead(200, {'Content-Type': 'text/html'});
@@ -14,13 +14,13 @@ http.createServer(function (req, res) {
             res.end("采集完成");
         });
     } else if (type == "fs" && id != null) {
-        lib.send(id, function () {
-            res.end("发送处理中..");
-        });
-    } else if (type == "err" && id != null) {
         lib.senderr(id, function () {
             res.end("发送处理中..");
         });
+    } else if (type == "status" && id != null) {
+        status.status(id, function (all, sended) {
+            res.end("总共:" + all + "     已发:" + sended);
+        })
     }
     else {
         res.end('hello world!');
